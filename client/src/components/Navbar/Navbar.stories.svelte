70b64109cd070b8d8c411f11d6a2dfbffc5ae82a
Steeveyboy <jonathan.qsteeves@gmail.com>
2021-05-01 14:53:20 -0400

    Navbar Made

---
+++ client/src/components/Navbar/Navbar.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+    import Navbar from './Navbar.svelte'
+</script>
+
+<Meta
+    title="Example/Navbar"
+    component={Navbar}
+/>
+
+<Template>
+    <Navbar/>
+</Template>
+
+<Story
+    name="COMP 1805"
+/>
