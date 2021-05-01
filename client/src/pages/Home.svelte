6f60770a00d525a6dfaed6a6551bd2b1b720142e
Steeveyboy <jonathan.qsteeves@gmail.com>
2021-05-01 14:17:44 -0400

    Created Button

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,6 +1,14 @@
 <script>
-    import { Navbar } from '../components'
+    import { Navbar, Button} from '../components'
 </script>
 
 <Navbar/>
-<h1>Home</h1>
+
+<!-- <h1>Home</h1> -->
+
+<div>
+    <h2>Carleton Computer Science Society</h2>
+    <h1>DiscereteMath.ca</h1>
+    <p>In publishing and lorem ipsum ipsum </p>
+    <div ><Button label="click me"/></div>
+</div>
