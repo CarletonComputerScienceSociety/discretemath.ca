85cba482f0c8eda2b89837d9ca2ef3df42d7fa83
Dami Adesola <damilola.adesola1@gmail.com>
2021-05-25 02:48:21 +0400

    Initialised MultipleChoiceQuestion Model with Controller and CourseSession Controller

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_171955) do
+ActiveRecord::Schema.define(version: 2021_05_24_222950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,17 @@ ActiveRecord::Schema.define(version: 2021_05_24_171955) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "multiple_choice_questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "body_format"
+    t.text "pseudocode"
+    t.bigint "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_multiple_choice_questions_on_course_id"
+  end
+
   add_foreign_key "course_sessions", "courses"
+  add_foreign_key "multiple_choice_questions", "courses"
 end
