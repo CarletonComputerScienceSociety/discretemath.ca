68b4f7543244028eb031de5b3ecbd55a56ae2b04
Matthew M-B <matthewmacraebovell@outlook.com>
2021-05-29 02:16:35 -0400

    course model test (#35)
    * course model tests
    
    * lint

--- api/spec/models/course_spec.rb
+++ api/spec/models/course_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe(Course, type: :model) do
+  let(:course) { build(:course) }
+
+  describe 'Course Attribute Validations' do
+    let(:result) { course.valid? }
+
+    context 'when course has no title' do
+      before { course.title = nil }
+
+      it 'returns false' do
+        expect(result).to(be(false))
+      end
+    end
+
+    context 'when course has no code' do
+      before { course.code = nil }
+
+      it 'returns false' do
+        expect(result).to(be(false))
+      end
+    end
+  end
 end
