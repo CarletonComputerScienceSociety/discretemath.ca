6464b4b732deccd05bb5af7155e47aee8fa9e5b1
MathyouMB <matthewmacraebovell@bell.net>
2021-05-29 00:09:35 -0400

    lint fixes

--- api/app/controllers/question_controller.rb
+++ api/app/controllers/question_controller.rb
@@ -1,40 +1,43 @@
 class QuestionController < ApplicationController
-    def index
-        @Question = Question.all
-    end
-    def show
-        @Question = Question.find(params[:id])
-    end
-    def new
-        @Question = Question.new
-    end
-    def create
-        @Question = Question.create(selection_params)
-        if @Question.save
-            flash[:success] = "Question successfully saved!"
-            redirect_to @Question
-        else
-            flash[:error] =  "Question was not saved!"
-            render: new
-        end
-    end
-    def update
-        @Question = Question.update(selection_params)
-        if @Question.save
-            flash[:success] = "Question successfully saved!"
-            redirect_to @Question
-        else
-            flash[:error] =  "Question was not saved!"
-            render: edit
-        end
-    end
-    def destroy
-        Question.find(params[:id]).destroy
-    end
+  def index
+    @question = Question.all
+  end
 
-    private
+  def show
+    @question = Question.find(params[:id])
+  end
 
-        def selection_params
-            params.require(:Question).permit(:title, :body, :body_format, :solution ,:solution_format, :course_id)
-        end
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(selection_params)
+    if @question.save
+      flash[:success] = 'Question successfully saved!'
+      redirect_to @question
+    else
+      flash[:error] = 'Question was not saved!'
+    end
+  end
+
+  def update
+    @question = Question.update(selection_params)
+    if @question.save
+      flash[:success] = 'Question successfully saved!'
+      redirect_to @question
+    else
+      flash[:error] = 'Question was not saved!'
+    end
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy
+  end
+
+  private
+
+  def selection_params
+    params.require(:Question).permit(:title, :body, :body_format, :solution, :solution_format, :course_id)
+  end
 end
