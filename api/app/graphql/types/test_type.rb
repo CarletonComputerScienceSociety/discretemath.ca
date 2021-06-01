7c3b61f7d93afa33a1189b16b36d36f6245bdc9d
Dami Adesola <damilola.adesola1@gmail.com>
2021-06-01 22:54:05 +0400

    Added Test Query and Type in graphql

---
+++ api/app/graphql/types/test_type.rb
@@ -0,0 +1,13 @@
+module Types
+  class TestType < Types::BaseObject
+    description 'A type that represents a Course entity.'
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :description, String, null: false
+    field :course_session, Types::CourseSessionType, null: true
+
+    def course_session
+      Loaders::AssociationLoader.for(Course, :course_session).load(object)
+    end
+  end
+end
