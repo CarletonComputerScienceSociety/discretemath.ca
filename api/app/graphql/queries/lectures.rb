fa7b3a96186f711b904411ae223e8193d0ef8cc1
Dami Adesola <damilola.adesola1@gmail.com>
2021-06-02 12:53:53 +0400

    Re-formatted files with Rubocop

--- api/app/graphql/queries/lectures.rb
+++ api/app/graphql/queries/lectures.rb
@@ -1,11 +1,10 @@
 module Queries
-    class Lectures < Queries::BaseQuery
-      description 'Get Lectures'
-      type [Types::LectureType], null: false
-  
-      def resolve
-        ::Lecture.all
-      end
+  class Lectures < Queries::BaseQuery
+    description 'Get Lectures'
+    type [Types::LectureType], null: false
+
+    def resolve
+      ::Lecture.all
     end
   end
-  
+end
