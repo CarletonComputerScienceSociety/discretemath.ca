88c1733a86d4717073edfb3c5e97b1c31edcc144
Damilola Adesola <63448215+DamiAdesola@users.noreply.github.com>
2021-06-14 23:36:51 +0400

    Started Client Integration (#40)
    Added new dependencies to connect the client-side to the backend

---
+++ client/src/data/queries/Test/Test.query.js
@@ -0,0 +1,29 @@
+//import {gql} from '@apollo/client';
+/*
+const TEST = gql`
+  query test($id: ID!) {
+    test(id: $id) {
+      description
+      id
+      testQuestions {
+        question {
+          ... on Question {
+            id
+            title
+            bodyFormat
+          }
+          ... on MultipleChoiceQuestion {
+            id
+            title
+            bodyFormat
+          }
+        }
+      }
+      title
+    }
+  }
+`;
+*/
+const TEST = "Hello";
+
+export {TEST};
