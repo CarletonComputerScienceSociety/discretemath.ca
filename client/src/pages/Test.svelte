9e560bc56faa5386d8ca125a2f4ba1ba46c2f9e0
Dami Adesola <damilola.adesola1@gmail.com>
2021-06-14 21:56:47 +0400

    Started Client Integration

---
+++ client/src/pages/Test.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {CourseNavbar as Navbar} from '../components';
+    import {query} from 'svelte-apollo';
+    import {TEST} from '../data';
+  </script>
+  
+  <Navbar />
+  <div class="course-page">
+    <h1>Test</h1>
+  </div>
+  
