44c68af322578c5f904563dedff9ff3f0e05ab10
eriicali <ericali3@cmail.carleton.ca>
2021-06-16 21:39:06 -0400

    created MultipleChoiceAnswer component

---
+++ client/src/components/MultipleChoiceAnswer/MultipleChoiceAnswer.stories.svelte
@@ -0,0 +1,70 @@
+<script>
+  import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+  import MultipleChoiceAnswer from './MultipleChoiceAnswer.svelte';
+</script>
+
+<Meta
+  title="Example/MultipleChoiceAnswer"
+  component={MultipleChoiceAnswer}
+  argTypes={{
+    letter: {options: ['a', 'b', 'c', 'd']},
+    body: {control: 'text'},
+    body_format: {options: ['mathjax', 'text', 'html']},
+    state: {options: ['unselected', 'selected', 'correct', 'incorrect', 'solution']},
+    onClick: {action: 'onClick'}
+  }}
+/>
+
+<Template let:args>
+  <MultipleChoiceAnswer {...args} on:click={args.onClick} />
+</Template>
+
+<Story
+  name="Answer 1"
+  args={{
+    letter: 'a',
+    body: '$$O(x^2)$$',
+    body_format: 'mathjax',
+    state: 'correct'
+  }}
+/>
+
+<Story
+  name="Answer 2"
+  args={{
+    letter: 'b',
+    body: '$$O(x^2)$$',
+    body_format: 'mathjax',
+    state: 'incorrect'
+  }}
+/>
+
+<Story
+  name="Answer 3"
+  args={{
+    letter: 'c',
+    body: '$$O(x^2)$$',
+    body_format: 'mathjax',
+    state: 'selected'
+  }}
+/>
+
+<Story
+  name="Answer 4"
+  args={{
+    letter: 'd',
+    body: '$$O(x^2)$$',
+    body_format: 'mathjax',
+    state: 'solution'
+  }}
+/>
+
+<Story
+  name="Answer 5"
+  args={{
+    letter: 'a',
+    body: '$$O(x^2)$$',
+    body_format: 'mathjax',
+    state: 'unselected'
+  }}
+/>
---
+++ client/src/components/MultipleChoiceAnswer/MultipleChoiceAnswer.svelte
@@ -0,0 +1,89 @@
+<script>
+  import {createEventDispatcher} from 'svelte';
+  import {onMount} from 'svelte';
+
+  // props
+  export let letter;
+  export let body;
+  export let body_format;
+  export let state;
+
+  
+  // selected: orange
+  // correct: green
+  // incorrect: red
+  // solution: light green
+  
+  onMount(() => {
+    let script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+    document.head.append(script);
+
+    script.onload = () => {
+      MathJax = {
+        tex: {
+          inlineMath: [
+            ['$', '$'],
+            ['\\(', '\\)']
+          ]
+        },
+        svg: {fontCache: 'global'}
+      };
+    };
+  });
+
+  
+</script>
+
+<div class="main {state}" id="answer" >
+  <div class="letter">
+    ({letter})
+  </div>
+  <div class="body">
+    {body}
+  </div>
+</div>
+
+<style>
+  .main {
+    height: 50px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    border-style: solid;
+    border-radius: 10px;
+    border-width: 1px;
+    border-color: #c0c0c0;
+    font-family: 'Nunito', sans-serif;
+  }
+
+  .unselected:hover {
+    cursor: pointer;
+    background-color: #edfeff;
+  }
+
+  .selected {
+    background-color: #99ccff;
+  }
+
+  .unselected {
+    background-color: white;
+  }
+
+  .correct {
+    background-color: #80ff80;
+    
+  }
+  .incorrect {
+    background-color: #ff5c33;
+    
+  }
+  .solution {
+    background-color: #c1f0c1;
+    
+  }
+
+  .letter {
+    margin: 10px;
+  }
+</style>
