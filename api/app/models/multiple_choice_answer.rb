eb9c49fc244f84dd6c70be9cf1c12998dfb2e6b9
MathyouMB <matthewmacraebovell@bell.net>
2021-06-17 00:01:36 -0400

    added multiple choice answers to api

--- api/app/models/multiple_choice_answer.rb
+++ api/app/models/multiple_choice_answer.rb
@@ -4,5 +4,6 @@ class MultipleChoiceAnswer < ApplicationRecord
   validates :body_format, presence: true
   validates :multiple_choice_question_id, presence: true
 
+  # Relations
   belongs_to :multiple_choice_question
 end
