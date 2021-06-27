3df03bf34d3b8b89e02c8be74dfe87ef84e2a01c
MathyouMB <matthewmacraebovell@bell.net>
2021-06-27 17:07:44 -0400

    test components

---
+++ client/src/components/MultipleChoiceQuestion/MultipleChoiceQuestion.svelte
@@ -0,0 +1,23 @@
+<script>
+  import {MultipleChoiceAnswer} from '../../components';
+  export let number;
+  export let body;
+  export let body_format;
+  export let answers;
+</script>
+
+<div class="multiple-choice-question">
+  question
+  {#each answers as answer}
+    <MultipleChoiceAnswer
+      letter={'a'}
+      body={answer.body}
+      body_format={answer.body_format}
+      correct={answer.correct}
+      state="unselected"
+    />
+  {/each}
+</div>
+
+<style type="text/scss">
+</style>
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,7 @@ export {Button} from './Button';
 export {CourseNavbar} from './CourseNavbar';
 export {Header} from './Header';
 export {HeaderInformation} from './HeaderInformation';
+export {MultipleChoiceAnswer} from './MultipleChoiceAnswer';
+export {MultipleChoiceQuestion} from './MultipleChoiceQuestion';
 export {Navbar} from './Navbar';
+export {Test} from './Test';
