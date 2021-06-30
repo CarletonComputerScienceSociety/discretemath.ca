1351b94190178ea7db8f9192068f3a549418428d
Dami Adesola <damilola.adesola1@gmail.com>
2021-06-30 22:02:52 +0400

    Added TrueOrFalseQuestion with Changes Requested

--- api/app/graphql/types/polymorphic_question_type.rb
+++ api/app/graphql/types/polymorphic_question_type.rb
@@ -1,7 +1,7 @@
 module Types
   class PolymorphicQuestionType < Types::BaseUnion
     description 'Various kinds of Test Questions'
-    possible_types Types::MultipleChoiceQuestionType, Types::QuestionType,, Types::TrueOrFalseQuestionType
+    possible_types Types::MultipleChoiceQuestionType, Types::QuestionType, Types::TrueOrFalseQuestionType
     def self.resolve_type(object, _context)
       case object
       when MultipleChoiceQuestion
@@ -14,4 +14,3 @@ module Types
       end
     end
   end
-end
