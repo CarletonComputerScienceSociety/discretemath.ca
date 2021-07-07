72a4693bb456fd2e7f87b7b7db89266a8244903a
Dami Adesola <damilola.adesola1@gmail.com>
2021-07-07 22:59:43 +0400

    Course Query with Validations

--- api/app/graphql/queries/course.rb
+++ api/app/graphql/queries/course.rb
@@ -6,22 +6,21 @@ module Queries
     type Types::CourseType, null: false
 
     def resolve(id: nil, code: nil)
-      if (code.nil?)
+      if code.nil?
         course = ::Course.find(id)
         return GraphQL::ExecutionError.new('ERROR: Course of given ID is nil') if course.nil?
-      
+
         course
-      elsif (id.nil?)
+      elsif id.nil?
         course = ::Course.find_by(code)
         return GraphQL::ExecutionError.new('ERROR: Course of given ID is nil') if course.nil?
 
         course
       else
-        return GraphQL::ExecutionError.new('ERROR: Both ID and Course Code given is nil') 
+        GraphQL::ExecutionError.new('ERROR: Both ID and Course Code given is nil')
       end
-
-      rescue ActiveRecord::RecordNotFound
-        GraphQL::ExecutionError.new('ERROR: Course of given ID is nil')
-      end
+    rescue ActiveRecord::RecordNotFound
+      GraphQL::ExecutionError.new('ERROR: Course of given ID is nil')
+    end
   end
 end
