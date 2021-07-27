2602e56672c07b1089bf45ebe4b9c15eb15347a2
MathyouMB <matthewmacraebovell@bell.net>
2021-07-27 01:45:43 -0400

    added minio setup

--- api/app/models/multiple_choice_question.rb
+++ api/app/models/multiple_choice_question.rb
@@ -9,4 +9,13 @@ class MultipleChoiceQuestion < ApplicationRecord
   # Relations
   has_many :multiple_choice_answers, dependent: :delete_all
   belongs_to :course
+  has_one_attached :image
+
+  # Methods
+  def image_url
+    Rails.cache.fetch([cache_key, __method__]) do
+      Rails.application.routes.url_helpers
+           .rails_blob_url(image, only_path: true)
+    end
+  end
 end
