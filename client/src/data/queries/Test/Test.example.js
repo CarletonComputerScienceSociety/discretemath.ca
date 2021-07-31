14b592b1550d4ef40c13cafc52527c458a055585
MathyouMB <matthewmacraebovell@bell.net>
2021-07-31 17:26:39 -0400

    update pseudocode mock data

--- client/src/data/queries/Test/Test.example.js
+++ client/src/data/queries/Test/Test.example.js
@@ -16,6 +16,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m01',
             body: 'Let $A$ be a set of size 7 and let $B$ be a set of size 13. How many one-to-one functions $f: A \\rightarrow B$ are there?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -54,6 +55,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m02',
             body: 'You are given 5 books and 7 bookshelves. How many ways are there to place these books on the shelves? (The order on the shelves matters.)',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -92,6 +94,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m03',
             body: 'A password consists of 6 or 7 characters, each character being an uppercase or a lowercase letter. A password must contain at least one uppercase letter. How many passwords are there?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -130,6 +133,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m04',
             body: 'In a group of 20 people,\n<ul>\n  <li>6 are blond,</li>\n  <li>7 have green eyes,</li>\n  <li>11 are not blond and do not have green eyes.</li>\n  </ul>\nHow many people are blond and have green eyes?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -168,6 +172,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m05',
             body: 'How many bitstrings of length 55 start with 101 or end with 1111?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -206,6 +211,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m06',
             body: 'Each person in a group of $n$ people has a last name consisting of two uppercase letters. For what values of $n$ can we guarantee that there are at least two people with the same last name?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -244,6 +250,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m07',
             body: 'How many bitstrings of length 13 contain exactly 3 zeros?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -282,6 +289,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m08',
             body: 'What is the coefficient of $x^{12}y^{12}$ in the expansion of $(3x-7y)^{24}$?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -320,6 +328,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m09',
             body: 'Which of the following is true?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -358,6 +367,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m10',
             body: 'How many strings can be obtained by rearranging the letters of the word\n<p align="center">POOPERSCOOPER</p>',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -396,6 +406,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m11',
             body: 'The function $f: \\mathbb{N} \\rightarrow \\mathbb{N}$ is defined by\n<p align="center">$f(0) = 14$</p>\n<p align="center">$f(n+1) = f(n) + 4n - 5$ for $n \\geq 0$</p>\nWhat is $f(n)$?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -432,8 +443,10 @@ const TEST_EXAMPLE_DATA = {
             __typename: 'MultipleChoiceQuestion',
             id: '4',
             title: 'f13m12',
-            body: 'Consider the recursive algorithm ${\\rm F{\\small IB}}$, which takes as input an integer $n \\geq 0$: <br><br>\n\n<div id="pseudocode"></div>\n<br>\n\n<pre id="fib-code" style="display:none;">\n\\begin{algorithmic}\n\\PROCEDURE{Fib}{$n$}\n\\IF{$n = 0$ or $n = 1$}\n  \\STATE $f = n$\n\\ELSE \\STATE $f = $ \\CALL{Fib}{$n - 1$} + \\CALL{Fib}{$n - 2$}\n\\ENDIF\n\\STATE return $f$\n\\ENDPROCEDURE\n\\end{algorithmic}\n</textarea>\n</pre>\n\nWhen running ${\\rm F{\\small IB}}(5)$, how many calls are there to ${\\rm F{\\small IB}}(2)$?',
+            body: 'Consider the recursive algorithm ${\\rm F{\\small IB}}$, which takes as input an integer $n \\geq 0$. When running ${\\rm F{\\small IB}}(5)$, how many calls are there to ${\\rm F{\\small IB}}(2)$?',
             bodyFormat: 'mathjax',
+            pseudocode:
+              '\n    \\begin{algorithmic}\n    \\PROCEDURE{Fib}{$n$}\n    \\IF{$n = 0$ or $n = 1$}\n        \\STATE $f = n$\n    \\ELSE \\STATE $f = $ \\CALL{Fib}{$n - 1$} + \\CALL{Fib}{$n - 2$}\n    \\ENDIF\n    \\STATE return $f$\n    \\ENDPROCEDURE\n    \\end{algorithmic}\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -470,8 +483,10 @@ const TEST_EXAMPLE_DATA = {
             __typename: 'MultipleChoiceQuestion',
             id: '10',
             title: 'f13m13',
-            body: 'The Fibonacci numbers are defined as follows: $f_0 = 0, f_1 = 1$, and $f_n = f_{n-1} + f_{n-2}$ for $n \\geq 2$. <br>\nConsider the recursive algorithm ${\\rm F{\\small IB}}$, which takes as input an integer $n \\geq 0$: <br><br>\n\n<div id="pseudocode"></div>\n<br>\n\n<pre id="fib-code" style="display:none;">\n\\begin{algorithmic}\n\\PROCEDURE{Fib}{$n$}\n\\IF{$n = 0$ or $n = 1$}\n  \\STATE $f = n$\n\\ELSE \\STATE $f = $ \\CALL{Fib}{$n - 1$} + \\CALL{Fib}{$n - 2$}\n\\ENDIF\n\\STATE return $f$\n\\ENDPROCEDURE\n\\end{algorithmic}\n</textarea>\n</pre>\n\nFor $n \\geq 2$, run algorithm ${\\rm F{\\small IB}}(n)$ and let $a_n$ be the number of times that ${\\rm F{\\small IB}}(0)$ is called.',
+            body: 'The Fibonacci numbers are defined as follows: $f_0 = 0, f_1 = 1$, and $f_n = f_{n-1} + f_{n-2}$ for $n \\geq 2$. <br>\nConsider the recursive algorithm ${\\rm F{\\small IB}}$, which takes as input an integer $n \\geq 0$. For $n \\geq 2$, run algorithm ${\\rm F{\\small IB}}(n)$ and let $a_n$ be the number of times that ${\\rm F{\\small IB}}(0)$ is called.',
             bodyFormat: 'mathjax',
+            pseudocode:
+              '\n    \\begin{algorithmic}\n    \\PROCEDURE{Fib}{$n$}\n    \\IF{$n = 0$ or $n = 1$}\n      \\STATE $f = n$\n    \\ELSE \\STATE $f = $ \\CALL{Fib}{$n - 1$} + \\CALL{Fib}{$n - 2$}\n    \\ENDIF\n    \\STATE return $f$\n    \\ENDPROCEDURE\n    \\end{algorithmic}\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -510,6 +525,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m14',
             body: 'What does the summation $\\sum_{k=7}^{n} {{k-1}\\choose{6}}$ count?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -548,6 +564,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m15',
             body: 'If you flip a fair coin 4 times, what is the probability that the coin comes up heads exactly twice?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -586,6 +603,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m16',
             body: 'If you choose an element $x$ uniformly at random from the set ${1, 2, ..., 100}$, what is the probability that $x$ is divisible by 4 or 5?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
@@ -624,6 +642,7 @@ const TEST_EXAMPLE_DATA = {
             title: 'f13m17',
             body: 'Consider a multiple choice midterm with 17 questions, in which for each question, four options are given to choose from. You answer each question by choosing an answer uniformly at random, and independently of the other answers. What is the probability that you get all answers correct?',
             bodyFormat: 'mathjax',
+            pseudocode: '\n\n',
             multipleChoiceAnswers: [
               {
                 __typename: 'MultipleChoiceAnswer',
