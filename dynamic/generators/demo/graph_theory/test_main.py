6d68d243c08b8989010b37309270beba036b7218
MathyouMB <matthewmacraebovell@bell.net>
2021-08-02 19:16:20 -0400

    format

--- dynamic/generators/demo/graph_theory/test_main.py
+++ dynamic/generators/demo/graph_theory/test_main.py
@@ -2,6 +2,4 @@ import main as generate_graph_theory_question
 
 
 def test():
-    assert generate_graph_theory_question.start() == {
-        "description": "test"
-    }
+    assert generate_graph_theory_question.start() == {"description": "test"}
