cda99bacf41513c2d4dbabfb94c6c9a6166f9a05
Forest Anderson <forestkzanderson@gmail.com>
2021-08-05 21:09:27 -0400

    allow host

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -104,4 +104,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.hosts << "api.discretemath.ca"
 end
