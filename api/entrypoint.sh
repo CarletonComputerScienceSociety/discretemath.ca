3bb3293cb7952fd2adbe8471ab1726726672ff36
Forest Anderson <forestkzanderson@gmail.com>
2021-08-05 21:02:21 -0400

    Back to dev

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -9,4 +9,4 @@ rm -f tmp/pids/server.pid
 bundle exec rails db:setup --trace
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-bundle exec rails s -p 3000 -b '0.0.0.0' -e production
+bundle exec rails s -p 3000 -b '0.0.0.0'
