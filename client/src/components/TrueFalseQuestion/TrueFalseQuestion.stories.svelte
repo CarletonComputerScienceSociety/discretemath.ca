85c9d2c3e4fef75c4095f89db94a8a5b71002107
Elias Hawa <elias.hawa2468@gmail.com>
2021-08-08 13:27:42 -0400

    Create true or false question component

---
+++ client/src/components/TrueFalseQuestion/TrueFalseQuestion.stories.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+  import TrueFalseQuestion from './TrueFalseQuestion.svelte';
+</script>
+
+<Meta
+  title="Example/TrueFalseQuestion"
+  component={TrueFalseQuestion}
+  argTypes={{
+    number: {control: 'number'},
+    body: {control: 'text'}
+  }}
+/>
+
+<Template let:args>
+  <TrueFalseQuestion {...args} />
+</Template>
+
+<Story
+  name="Example one"
+  args={{
+    number: 18,
+    body: '1+1=7'
+  }}
+/>
---
+++ client/src/components/TrueFalseQuestion/TrueFalseQuestion.svelte
@@ -0,0 +1,69 @@
+<script>
+  export let number;
+  export let body;
+  export let bodyFormat;
+</script>
+
+<div class="true-false-question">
+  <div>{number}. {body}</div>
+  <div class="true-false-container">
+    <div class="true-false-option">
+      <input type="radio" name="question{number}" id="true{number}" />
+      <label for="true{number}">True</label>
+    </div>
+    <div class="true-false-option">
+      <input type="radio" name="question{number}" id="fasle{number}" />
+      <label for="fasle{number}">Fasle</label>
+    </div>
+    <!-- Change value="true" to onclick function -->
+  </div>
+</div>
+
+<style type="text/css">
+  .true-false-question {
+    font-family: 'Nunito', sans-serif;
+    padding: 1rem;
+  }
+
+  .true-false-container{
+      margin-top: 1rem;
+      display: flex;
+      gap: 10rem;
+  }
+
+  .true-false-option{
+      position: relative;
+  }
+
+  .true-false-option input,
+  .true-false-option label {
+    position: absolute;
+    cursor: pointer;
+    background-color: white;
+  }
+
+  .true-false-option input{
+      display: none;
+      opacity: 0;
+  }
+
+  .true-false-option input[type="radio"]:checked + label{
+      background-color: #99ccff;
+  }
+
+  .true-false-option label {
+    height: 2.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-style: solid;
+    border-radius: 0.6rem;
+    border-width: 0.1rem;
+    border-color: #c0c0c0;
+    width: 8rem;
+  }
+
+  .true-false-option label:hover{
+      background-color: #edfeff;
+  }
+</style>
