58488d42f9416c7687b4d736d2b6ef19e2d11972
Elias Hawa <elias.hawa2468@gmail.com>
2021-08-11 14:36:23 -0400

    Create CheckQuestion Component

---
+++ client/src/components/CheckQuestion/CheckQuestion.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+  import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+  import CheckQuestion from './CheckQuestion.svelte';
+</script>
+
+<Meta
+  title="Example/CheckQuestion"
+  component={CheckQuestion}
+  argTypes={{
+    number: {control: 'number'},
+    body: {control: 'text'},
+    options: {control: 'array'}
+  }}
+/>
+
+<Template let:args>
+  <CheckQuestion {...args} />
+</Template>
+
+<Story
+  name="Example one"
+  args={{
+    number: 1,
+    body: 'Consider the set S = {n in Z: 0 <= n <= 20 and n mod 5 = n mod 7}. Select all of the elements x in S',
+    options: ['0', '1', '2', '3', '4', '5', '6', '7']
+  }}
+/>
