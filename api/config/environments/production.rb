817298753f08fbf4433dfa5df96229d2525b2471
Forest Anderson <forestkzanderson@gmail.com>
2021-08-11 21:16:22 -0400

    Changed storage

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
 
-  config.active_storage.service = :minio
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
