dcff33ce4bf4675067d7f860c2cc961cac665e85
Forest Anderson <forestkzanderson@gmail.com>
2021-08-11 20:05:40 -0400

    Change exec to prod

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -6,7 +6,7 @@ env
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f tmp/pids/server.pid
 
-bundle exec rails db:setup --trace
+bundle exec rails -e production db:setup --trace
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 bundle exec rails s -p 3000 -b '0.0.0.0' -e production
