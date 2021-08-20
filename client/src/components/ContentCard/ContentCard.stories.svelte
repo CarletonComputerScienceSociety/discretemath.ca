35870395e8a11a30f2cb91cc6546ac6eb0d279f7
Elias Hawa <elias.hawa2468@gmail.com>
2021-08-19 21:22:56 -0400

    Create content card component

---
+++ client/src/components/ContentCard/ContentCard.stories.svelte
@@ -0,0 +1,68 @@
+<script>
+  import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+  import ContentCard from './ContentCard.svelte';
+</script>
+
+<Meta
+  title="Example/ContentCard"
+  component={ContentCard}
+  argTypes={{
+    title: {control: 'text'},
+    info: {control: 'text'},
+    tag: {control: 'text'}
+  }}
+/>
+
+<Template let:args>
+  <ContentCard {...args} />
+</Template>
+
+<Story
+  name="Lecture example"
+  args={{
+    title: 'Sequences and Sums',
+    info: 'Yesterday lol',
+    tag: 'COMP 1805',
+    type: 'Lecture'
+  }}
+/>
+
+<Story
+  name="Drill example"
+  args={{
+    title: 'Lecture 5 Drill',
+    info: '5',
+    tag: 'COMP 1805',
+    type: 'Drill'
+  }}
+/>
+
+<Story
+  name="Test example"
+  args={{
+    title: 'Functions Test',
+    info: '20',
+    tag: 'COMP 1805',
+    type: 'Test'
+  }}
+/>
+
+<Story
+  name="Midterm example"
+  args={{
+    title: 'Mock Midterm 2021',
+    info: '50',
+    tag: 'COMP 1805',
+    type: 'Midterm'
+  }}
+/>
+
+<Story
+  name="Exam example"
+  args={{
+    title: 'Final Exam 2020',
+    info: '50',
+    tag: 'COMP 1805',
+    type: 'Exam'
+  }}
+/>
---
+++ client/src/components/ContentCard/ContentCard.svelte
@@ -0,0 +1,67 @@
+<script>
+  export let title;
+  export let info;
+  export let tag;
+  export let type;
+
+  import Fa from 'svelte-fa';
+  import {
+    faQuestion, //Questions
+    faCalendarAlt, // Date
+    faChalkboard, //Lecture / lesson
+    faLightbulb, //Drill
+    faClipboard, //Test
+    faPencilAlt, //Midterm
+    faPenAlt //Exam
+  } from '@fortawesome/free-solid-svg-icons';
+
+  const tagIcons = {
+    Lecture: faChalkboard,
+    Drill: faLightbulb,
+    Test: faClipboard,
+    Midterm: faPencilAlt,
+    Exam: faPenAlt
+  };
+
+  const infoIcons = {
+    Lecture: faCalendarAlt,
+    Drill: faQuestion,
+    Test: faQuestion,
+    Midterm: faQuestion,
+    Exam: faQuestion
+  };
+</script>
+
+<div class="outer-test-card">
+  <div class="tag"><Fa icon={tagIcons[type]} /> {tag}</div>
+  <div class="test-card-title">{title}</div>
+  <div class="info"><Fa icon={infoIcons[type]} /> {info}</div>
+</div>
+
+<style>
+  .outer-test-card {
+    font-family: 'Tahoma';
+    padding: 15px;
+    width: 150px;
+    height: 100px;
+    border-radius: 11%;
+    box-shadow: 0 0px 4px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
+  }
+
+  .tag {
+    margin-bottom: 10px;
+    color: #ffab04;
+  }
+
+  .test-card-title {
+    font-weight: bold;
+    font-size: 17px;
+    margin-bottom: 10px;
+    min-height: 42px;
+  }
+
+  .info {
+    margin-bottom: 5px;
+    color: #6c717a;
+  }
+</style>
