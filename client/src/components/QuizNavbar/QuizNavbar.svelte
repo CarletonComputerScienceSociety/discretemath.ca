148b00e9416a3a24c8cf01fc022b98ab12e50092
eriicali <ericali3@cmail.carleton.ca>
2021-08-25 19:18:41 -0400

    QuizNavbarQuestion component
    ran formatter

---
+++ client/src/components/QuizNavbar/QuizNavbar.svelte
@@ -0,0 +1,64 @@
+<script>
+  import {QuizNavbarQuestion} from './QuizNavbarQuestion';
+  export let questions;
+  export let model;
+</script>
+
+<div class="main">
+  <div class="title">QUIZ</div>
+  {#each questions as question}
+    <QuizNavbarQuestion
+      number={question.number}
+      state={question.state}
+      {model}
+    />
+  {/each}
+</div>
+
+<style type="text/scss">
+  .main {
+    height: auto;
+    width: 16%;
+    align-items: center;
+    border-style: solid;
+    /* border-radius: 0.6rem;*/
+    border-width: 0.1rem;
+    border-color: white;
+    margin: 0.5rem;
+    padding: 1rem;
+    display: grid;
+    grid-template-columns: auto auto auto auto auto;
+    align-items: center;
+    justify-content: center;
+    background-color: #ffab40;
+  }
+
+  .title {
+    text-align: center;
+    font-family: 'Assistant', sans-serif;
+    color: white;
+    font-weight: bold;
+    width: 1rem;
+    padding: 1rem;
+  }
+  @media only screen and (max-width: 1900px) {
+    .main {
+      grid-template-columns: auto auto auto auto;
+    }
+  }
+  @media only screen and (max-width: 1600px) {
+    .main {
+      grid-template-columns: auto auto auto;
+    }
+  }
+  @media only screen and (max-width: 1300px) {
+    .main {
+      grid-template-columns: auto auto;
+    }
+  }
+  @media only screen and (max-width: 920px) {
+    .main {
+      grid-template-columns: auto;
+    }
+  }
+</style>
---
+++ client/src/components/QuizNavbar/QuizNavbarQuestion/QuizNavbarQuestion.stories.svelte
@@ -0,0 +1,35 @@
+<script>
+  import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+  import QuizNavbarQuestion from './QuizNavbarQuestion.svelte';
+</script>
+
+<Meta
+  title="Example/QuizNavbarQuestion"
+  component={QuizNavbarQuestion}
+  argTypes={{
+    number: {control: 'number'},
+    state: {
+      options: ['unanswered', 'answered']
+    }
+  }}
+/>
+
+<Template let:args>
+  <QuizNavbarQuestion {...args} />
+</Template>
+
+<Story
+  name="Question 14"
+  args={{
+    number: 14,
+    state: 'unanswered'
+  }}
+/>
+
+<Story
+  name="Question 7"
+  args={{
+    number: 7,
+    state: 'answered'
+  }}
+/>
