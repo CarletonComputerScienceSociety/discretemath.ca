c3bcf10c4b46eb345f12b3d0006dc80e80e83e4d
NoahTheCorgi <91349811+NoahTheCorgi@users.noreply.github.com>
2021-10-02 20:06:23 -0400

    basic books in shelves code

--- dynamic/router.py
+++ dynamic/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 import generators.demo.graph_theory.main as graph_theory_question_generator
+import generators.booksinshelves.main as booksinshelves1_question_generator
 
 router = APIRouter(
     prefix="/api", tags=["generate"], responses={404: {"description": "Not found"}}
@@ -9,3 +10,7 @@ router = APIRouter(
 @router.get("/demo/graph-theory")
 async def generate_graph_theory_question():
     return graph_theory_question_generator.call()
+
+@router.get("/booksinshelves")
+async def generate_booksinshelves1_question():
+    return booksinshelves1_question_generator.call()
