efb8cda77e06bb67d0e105c0c54f2454f3d74e7b
NoahTheCorgi <91349811+NoahTheCorgi@users.noreply.github.com>
2021-10-03 11:49:07 -0400

    Updated test_main so that it actually tests the properly asserts the proper question and answers dictionary

--- dynamic/generators/booksinshelves/test_main.py
+++ dynamic/generators/booksinshelves/test_main.py
@@ -1,6 +1,65 @@
 import main
-from .main import call
+#from .main import call
 
 
 def test():
-    assert call() != None
+    out = main.call()
+    assert out == {
+        "title": "books in shelves",
+        "body": main.question,
+        "bodyFormat": "text",
+        "pseudocode": "",
+        "multipleChoiceAnswers": [
+            {
+                "body": main.answerchoices[0],
+                "bodyFormat": "mathjax",
+                "correct": "true",
+            },
+            {
+                "body": main.answerchoices[1],
+                "bodyFormat": "mathjax",
+                "correct": "false",
+            },
+            {
+                "body": main.answerchoices[2],
+                "bodyFormat": "mathjax",
+                "correct": "false",
+            },
+            {
+                "body": main.answerchoices[3],
+                "bodyFormat": "mathjax",
+                "correct": "false",
+            }
+        ],
+    }
+
+    print({
+        "title": "books in shelves",
+        "body": main.question,
+        "bodyFormat": "text",
+        "pseudocode": "",
+        "multipleChoiceAnswers": [
+            {
+                "body": main.answerchoices[0],
+                "bodyFormat": "mathjax",
+                "correct": "true",
+            },
+            {
+                "body": main.answerchoices[1],
+                "bodyFormat": "mathjax",
+                "correct": "false",
+            },
+            {
+                "body": main.answerchoices[2],
+                "bodyFormat": "mathjax",
+                "correct": "false",
+            },
+            {
+                "body": main.answerchoices[3],
+                "bodyFormat": "mathjax",
+                "correct": "false",
+            }
+        ],
+    })
+
+#test()
