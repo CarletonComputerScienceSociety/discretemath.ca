112f2e67dac9f797979950d3bc254af85c59b0d1
JoshuaSaikali <joshuasaikali@cmail.carleton.com>
2021-10-23 20:32:21 -0400

    Cleaned up main.py

--- dynamic/generators/comp2804/bitstrings_of_length/main.py
+++ dynamic/generators/comp2804/bitstrings_of_length/main.py
@@ -1,20 +1,19 @@
 import random
 
+
 def generate_question():
 
     question_body = " "
 
-    bitstring_length = random.randint(15, 99) # length of bitstring
-    numA = random.randint(0, 3) # position in ran_starts[]
-    numB = random.randint(0, 3) # position in ran_ends[]
+    bitstring_length = random.randint(15, 99)
 
-    ran_starts = ["01", "101", "1010", "01101"]
-    ran_ends = ["10", "010", "1010", "00101"]
+    rand_starts = random.choice(["01", "101", "1010", "01101"])
+    rand_ends = random.choice(["10", "010", "1010", "00101"])
 
-    length_start = len(ran_starts[numA]) # length of start
-    length_end = len(ran_ends[numB]) # length of end
+    length_start = len(rand_starts)
+    length_end = len(rand_ends)
 
-    question_body = "How many bitstrings of length " + str(bitstring_length) + " are there that start with " + ran_starts[numA] + " or end with " + ran_ends[numB] + "?"
+    question_body = f"How many bitstrings of length {bitstring_length} are there that start with {rand_starts} or end with {rand_ends}?"
 
     return {
         "title": "bitstrings of length",
@@ -45,17 +44,32 @@ def generate_question():
         ],
     }
 
+
 def answerchoice_1(bitstring_length, length_start, length_end):
-    return "$2^{" + str(bitstring_length - length_start) + "} + 2^{" + str(bitstring_length - length_end) + "} - 2^{" + str(bitstring_length - (length_start + length_end)) + "}$"
+    return "$2^{{{first_part_len}}} + 2^{{{second_part_len}}} - 2^{{{third_part_len}}}$".format(
+        first_part_len=(bitstring_length - length_start),
+        second_part_len=(bitstring_length - length_end),
+        third_part_len=(bitstring_length - (length_start + length_end)),)
+
 
 def answerchoice_2(bitstring_length, length_start, length_end):
-    return "$2^{" + str(bitstring_length) + "} - 2^{" + str(bitstring_length - length_end) + "} - 2^{" + str(bitstring_length - (length_start)) + "}$"
+    return "$2^{{{first_part_len}}} - 2^{{{second_part_len}}} - 2^{{{third_part_len}}}$".format(
+        first_part_len=bitstring_length,
+        second_part_len=(bitstring_length - length_end),
+        third_part_len=(bitstring_length - length_start),)
+
 
 def answerchoice_3(bitstring_length, length_start):
-    return "$2^{" + str(bitstring_length - length_start) + "} - 2^{" + str(bitstring_length) + "}$"
+    return "$2^{{{first_part_len}}} - 2^{{{second_part_len}}}$".format(
+        first_part_len=(bitstring_length - length_start),
+        second_part_len=bitstring_length,)
+
 
 def answerchoice_4(bitstring_length, length_start, length_end):
-    return "$2^{" + str(bitstring_length) + "} + 2^{" + str(bitstring_length + (length_start + length_end)) + "}$"
+    return "$2^{{{first_part_len}}} + 2^{{{second_part_len}}}$".format(
+        first_part_len=bitstring_length,
+        second_part_len=(bitstring_length + (length_start + length_end)),)
+
 
 def call():
     return generate_question()
