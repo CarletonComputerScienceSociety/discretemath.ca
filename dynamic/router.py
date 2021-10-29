7d8d88a1a884918eabc4340a8a9a3aebb71d5b3c
Sheikh Fahim Anwar <sheikhfahimanwar@Sheikhs-MacBook-Pro.local>
2021-10-29 04:19:50 -0400

    Added number of functions question generator (#121)

--- dynamic/router.py
+++ dynamic/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 import generators.demo.graph_theory.main as graph_theory_question_generator
+import generators.comp2804.num_of_functions.main as num_of_functions_generator
 
 router = APIRouter(
     prefix="/api", tags=["generate"], responses={404: {"description": "Not found"}}
@@ -9,3 +10,11 @@ router = APIRouter(
 @router.get("/demo/graph-theory")
 async def generate_graph_theory_question():
     return graph_theory_question_generator.call()
+
+
+## Number of Functions Question Generator
+@router.get("/comp2804/num-of-functions")
+async def generate_num_of_functions_question(
+    lower_range: int = 0, upper_range: int = 10
+):
+    return num_of_functions_generator.call(lower_range, upper_range)
