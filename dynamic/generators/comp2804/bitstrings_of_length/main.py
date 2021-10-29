e703f65665d12cac0befb846b00e3d5272fcace2
Joshua Saikali <91435428+JoshuaSaikali@users.noreply.github.com>
2021-10-29 12:22:45 -0400

    added bitstring generator (#123)
    * added bitstring generator
    
    * edits to the math in the answers
    
    * edit on router file
    
    * edits to the math in the answers
    
    * Fixed variables/added functions for answers
    
    * Edit to unused parameter
    
    * Update dynamic/generators/comp2804/bitstrings_of_length/main.py
    
    Co-authored-by: Forest Anderson <forestkzanderson@gmail.com>
    
    * Cleaned up main.py
    
    * format with black
    
    * make folder python package
    
    * add tests for answer methods
    
    * remove empty string
    
    Co-authored-by: JoshuaSaikali <joshuasaikali@cmail.carleton.com>
    Co-authored-by: Forest Anderson <forestkzanderson@gmail.com>
    Co-authored-by: Matthew MacRae-Bovell <matthew.macraebovell@shopify.com>

---
+++ dynamic/generators/comp2804/bitstrings_of_length/main.py
@@ -0,0 +1,77 @@
+import random
+
+
+def generate_question():
+
+    bitstring_length = random.randint(15, 99)
+
+    rand_starts = random.choice(["01", "101", "1010", "01101"])
+    rand_ends = random.choice(["10", "010", "1010", "00101"])
+
+    length_start = len(rand_starts)
+    length_end = len(rand_ends)
+
+    question_body = f"How many bitstrings of length {bitstring_length} are there that start with {rand_starts} or end with {rand_ends}?"
+
+    return {
+        "title": "bitstrings of length",
+        "body": question_body,
+        "body_format": "text",
+        "pseudocode": "",
+        "multiple_choice_answers": [
+            {
+                "body": answerchoice_1(bitstring_length, length_start, length_end),
+                "body_format": "mathjax",
+                "correct": "true",
+            },
+            {
+                "body": answerchoice_2(bitstring_length, length_start, length_end),
+                "body_format": "mathjax",
+                "correct": "false",
+            },
+            {
+                "body": answerchoice_3(bitstring_length, length_start),
+                "body_format": "mathjax",
+                "correct": "false",
+            },
+            {
+                "body": answerchoice_4(bitstring_length, length_start, length_end),
+                "body_format": "mathjax",
+                "correct": "false",
+            },
+        ],
+    }
+
+
+def answerchoice_1(bitstring_length, length_start, length_end):
+    return "$2^{{{first_part_len}}} + 2^{{{second_part_len}}} - 2^{{{third_part_len}}}$".format(
+        first_part_len=(bitstring_length - length_start),
+        second_part_len=(bitstring_length - length_end),
+        third_part_len=(bitstring_length - (length_start + length_end)),
+    )
+
+
+def answerchoice_2(bitstring_length, length_start, length_end):
+    return "$2^{{{first_part_len}}} - 2^{{{second_part_len}}} - 2^{{{third_part_len}}}$".format(
+        first_part_len=bitstring_length,
+        second_part_len=(bitstring_length - length_end),
+        third_part_len=(bitstring_length - length_start),
+    )
+
+
+def answerchoice_3(bitstring_length, length_start):
+    return "$2^{{{first_part_len}}} - 2^{{{second_part_len}}}$".format(
+        first_part_len=(bitstring_length - length_start),
+        second_part_len=bitstring_length,
+    )
+
+
+def answerchoice_4(bitstring_length, length_start, length_end):
+    return "$2^{{{first_part_len}}} + 2^{{{second_part_len}}}$".format(
+        first_part_len=bitstring_length,
+        second_part_len=(bitstring_length + (length_start + length_end)),
+    )
+
+
+def call():
+    return generate_question()
--- dynamic/router.py
+++ dynamic/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 import generators.demo.graph_theory.main as graph_theory_question_generator
+import generators.comp2804.bitstrings_of_length.main as bitstrings_of_length_generator
 
 router = APIRouter(
     prefix="/api", tags=["generate"], responses={404: {"description": "Not found"}}
@@ -9,3 +10,8 @@ router = APIRouter(
 @router.get("/demo/graph-theory")
 async def generate_graph_theory_question():
     return graph_theory_question_generator.call()
+
+
+@router.get("/comp2804/bitstrings-of-length")
+async def bitstrings_of_length_question():
+    return bitstrings_of_length_generator.call()
