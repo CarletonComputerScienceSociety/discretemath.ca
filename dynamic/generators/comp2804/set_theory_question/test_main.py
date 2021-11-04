0134e521f6ce2b35b0f520df0b4d8c8b592b5ca2
Sailesh Polavarapu <saileshpola@yahoo.ca>
2021-11-04 18:16:08 -0400

    added tests

--- dynamic/generators/comp2804/set_theory_question/test_main.py
+++ dynamic/generators/comp2804/set_theory_question/test_main.py
@@ -1,4 +1,9 @@
-from .main import call, calculate_sub_group
+from .main import (
+    call,
+    calculate_sub_group,
+    calculate_answer,
+    calculate_incorrect_answer_options,
+)
 
 
 def test_calculate_sub_group():
@@ -14,3 +19,35 @@ def test_calculate_sub_group():
 
     # assert test is correct
     assert calculated_sub_group == expected_sub_group
+
+
+def test_calculate_answer():
+    # provided data
+    total_number_of_people = 20
+    cond_one = 7
+    cond_two = 8
+    number_of_not_blonde_or_green_eyes = 11
+
+    expected_sub_group = total_number_of_people - number_of_not_blonde_or_green_eyes
+    expected_answer = (
+        expected_sub_group
+        - (expected_sub_group - cond_one)
+        - (expected_sub_group - cond_two)
+    )
+
+    # stores return value
+    calculated_answer = calculate_answer(expected_sub_group, cond_one, cond_two)
+
+    assert calculated_answer == expected_answer
+
+
+def test_calculate_incorrect_answer_options():
+    correct_answer = 3
+    incorrect_answer_options = calculate_incorrect_answer_options(correct_answer)
+
+    assert incorrect_answer_options == [4, 5, 2]
+
+    correct_answer = 0
+    incorrect_answer_options = calculate_incorrect_answer_options(correct_answer)
+
+    assert incorrect_answer_options == [1, 2, 3]
