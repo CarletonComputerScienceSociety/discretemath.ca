fa0ad9d4f446bb3b7da979cd24b740daeaaeaab2
saileshp56 <86889128+saileshp56@users.noreply.github.com>
2022-01-28 12:44:03 -0500

    Binomial expansion coefficient question (#133)
    * added binomial expansion question ws/#/tests/13 #4

---
+++ dynamic/generators/comp2804/binomial_expansion_coefficient/main.py
@@ -0,0 +1,69 @@
+import random
+
+
+def generate_question():
+
+    a_coefficient = random.randint(2, 5)
+    b_coefficient = random.randint(3, 7)
+
+    x_exponent = random.randint(3, 8)
+    y_exponent = random.randint(5, 11)
+
+    n = random.randint(11, 15)
+
+    answer = f"-\genfrac(){{n}}{{y_exponent}}\cdot{a_coefficient}^{x_exponent}\cdot{b_coefficient}^{y_exponent}"
+
+    question_body = f"What is the coefficient of x^{x_exponent}y^{y_exponent} in the expansion of ({a_coefficient} - {b_coefficient}y)^{n}"
+
+    return {
+        "title": "bitstrings of length",
+        "body": question_body,
+        "body_format": "text",
+        "pseudocode": "",
+        "multiple_choice_answers": [
+            {
+                "body": answerchoice_1(
+                    n, a_coefficient, b_coefficient, x_exponent, y_exponent
+                ),
+                "body_format": "mathjax",
+                "correct": "false",
+            },
+            {
+                "body": answer,
+                "body_format": "mathjax",
+                "correct": "true",
+            },
+            {
+                "body": answerchoice_3(
+                    n, a_coefficient, b_coefficient, x_exponent, y_exponent
+                ),
+                "body_format": "mathjax",
+                "correct": "false",
+            },
+            {
+                "body": answerchoice_4(
+                    n, a_coefficient, b_coefficient, x_exponent, y_exponent
+                ),
+                "body_format": "mathjax",
+                "correct": "false",
+            },
+        ],
+    }
+
+
+def answerchoice_1(n, a_coefficient, b_coefficient, x_exponent, y_exponent):
+    return f"\genfrac(){{n}}{{y_exponent}}\cdot{a_coefficient}^{x_exponent}\cdot{b_coefficient}^{y_exponent}"
+
+    # answerchoice_2 purposefully omitted
+
+
+def answerchoice_3(n, a_coefficient, b_coefficient, x_exponent, y_exponent):
+    return f"\genfrac(){{n}}{{y_exponent}}\cdot{a_coefficient}^{y_exponent}\cdot{b_coefficient}^{x_exponent}"
+
+
+def answerchoice_4(n, a_coefficient, b_coefficient, x_exponent, y_exponent):
+    return f"-\genfrac(){{n}}{{y_exponent}}\cdot{a_coefficient}^{y_exponent}\cdot{b_coefficient}^{x_exponent}"
+
+
+def call():
+    return generate_question()
--- dynamic/router.py
+++ dynamic/router.py
@@ -8,7 +8,10 @@ import generators.books_shelves.books_shelves_3.main as books_shelves_3_generato
 
 import generators.comp2804.set_theory_question.main as set_theory_question_generator
 import generators.comp2804.num_of_functions.main as num_of_functions_generator
+import generators.comp2804.let_m_and_n.main as m_and_n_generator
 import generators.comp2804.bitstrings_of_length.main as bitstrings_of_length_generator
+import generators.comp2804.binomial_expansion_coefficient.main as binomial_expansion_coefficient_generator
+
 
 router = APIRouter(
     prefix="/api", tags=["generate"], responses={404: {"description": "Not found"}}
@@ -24,8 +27,10 @@ routes = {
     },
     "comp2804": {
         "bitstrings-of-length": "/comp2804/bitstrings-of-length",
+        "binomial-expansion-coefficient": "/comp2804/binomial-expansion-coefficient",
         "set-theory-question": "/comp2804/set-theory",
         "num-of-functions": "/comp2804/num-of-functions",
+        "let-m-and-n-question": "/comp2804/let-m-and-n",
     },
 }
 
@@ -76,6 +81,15 @@ async def generate_num_of_functions_question(
     return num_of_functions_generator.call(lower_range, upper_range)
 
 
+@router.get(routes["comp2804"]["let-m-and-n-question"])
+async def generate_m_and_n_question():
+    return m_and_n_generator.call()
+
+
+@router.get(routes["comp2804"]["binomial-expansion-coefficient"])
+async def generate_m_and_n_question():
+    return binomial_expansion_coefficient_generator.call()
+
 @router.get(routes["comp2804"]["bitstrings-of-length"])
-async def bitstrings_of_length_question():
+async def generate_bitstrings_of_length_question():
     return bitstrings_of_length_generator.call()
