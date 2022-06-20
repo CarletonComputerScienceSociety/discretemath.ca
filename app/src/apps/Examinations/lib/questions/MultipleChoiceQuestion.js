92684fa92390022293914b701a501dc6f04d092c
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-19 21:00:14 -0400

    Add implementation for the MultipleChoiceQuestion class

--- app/src/apps/Examinations/lib/questions/MultipleChoiceQuestion.js
+++ app/src/apps/Examinations/lib/questions/MultipleChoiceQuestion.js
@@ -2,32 +2,53 @@ import { Question } from "../Question";
 
 class MultipleChoiceQuestion extends Question {
   selectedOptionIndex = -1;
+  options;
 
   // TODO: update foo to common question properties once Question contructor updated
-  constructor(foo, options) {
+  constructor(b, n, o) {
     // TODO: init options as MultipleChoiceOption once that class exists
-    super(foo);
+    super(b, n);
+    // add MultipleChoiceOptions
+    o.forEach((option) => {
+      this.addMultipleChoiceOption(
+        option.body,
+        option.correct,
+        option.letter,
+        option.index
+      );
+    });
+  }
+
+  // construct and add new MultipleChoiceOption
+  addMultipleChoiceOption(b, c, l, i) {
+    this.options.push(new MultipleChoiceOption(b, c, l, i));
   }
 
   updateOption(event) {
     // TODO: should update the selected option index
-    throw "Not implemented";
+    // MultipleChoiceOption component will dispatch the event containing its index
+    this.selectedOptionIndex = event.detail.index;
   }
 
   isAnsweredCorrectly() {
     // TODO: should return true if multipleChoiceOptions[selectedOptionIndex].correct is true
-    throw "Not implemented";
+    return options[this.selectedOptionIndex].correct;
   }
 }
 
 // TODO: implement this class
+class MultipleChoiceOption {
+  body;
+  correct;
+  letter;
+  index;
 
-// class MultipleChoiceOption {
-//     body;
-//     correct;
-//     constructor(foo) {
-
-//     }
-// }
+  constructor(b, c, l, i) {
+    this.body = b;
+    this.correct = c;
+    this.letter = l;
+    this.index = i;
+  }
+}
 
 export { MultipleChoiceQuestion };
