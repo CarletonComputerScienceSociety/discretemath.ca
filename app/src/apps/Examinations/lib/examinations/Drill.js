60f4d0b8406cb373fea3a838731c0b757a74c198
RohanSreelesh <rohansreelesh02@gmail.com>
2022-06-23 00:13:46 +0530

    drill class

--- app/src/apps/Examinations/lib/examinations/Drill.js
+++ app/src/apps/Examinations/lib/examinations/Drill.js
@@ -1,4 +1,16 @@
-// Drill extands Examination
+// Drill extends Examination
 // has array of questions
 // randomizes order of questions when they are added
 // has a "current question index"
+class Drill extends Examination{
+    constructor(title, author, questions){
+        super(title, author);
+        this.questions = questions;
+        this.currentQuestionIndex = -1;
+    }
+    
+    randomizeQuestions(questions){
+        questions.sort(() => Math.random() - 0.5);
+        return questions;
+    }
+}
