d5a0f97081e4c2d281b7f70c879dae06ae6985c2
Nguyen-Hanh Nong <nongnguyenhanh@gmail.com>
2022-07-15 13:37:20 -0400

    initial commit (created factory for one-to-one) (#181)
    * initial commit (created factory for one-to-one)
    
    * ran npm run format on the code
    
    Co-authored-by: Matthew M-B <matthewmacraebovell@outlook.com>

--- app/src/apps/Examinations/lib/factories/index.js
+++ app/src/apps/Examinations/lib/factories/index.js
@@ -1,2 +1,3 @@
 export { HowManyFunctions } from "./HowManyFunctions";
+export { HowManyOneToOneFunctions } from "./HowManyOneToOneFunctions";
 export { HowManyNonOneToOneFunctions } from "./HowManyNonOneToOneFunctions";
