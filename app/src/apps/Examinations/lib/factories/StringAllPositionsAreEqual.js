2bebffb18d8e0e450f54d34ac9cf29683c1ddd06
Nguyen-Hanh Nong <nongnguyenhanh@gmail.com>
2022-08-03 17:49:09 +0000

    Created SelectManyQuestions.js

--- app/src/apps/Examinations/lib/factories/StringAllPositionsAreEqual.js
+++ app/src/apps/Examinations/lib/factories/StringAllPositionsAreEqual.js
@@ -46,16 +46,16 @@ class StringAllPositionsAreEqual extends Factory {
     if (this.checkOdd(setASize) == true) {
       //If the question is about an odd position
       if (even_or_odd == "odd") {
-        return (2^(((setASize - 1)/2)+1));
+        return 2 ^ ((setASize - 1) / 2 + 1);
       }
       //If question is about an even position
       else {
-        return (2^(((setASize + 1)/2)+1));
+        return 2 ^ ((setASize + 1) / 2 + 1);
       }
     }
     //If set A is even
     else {
-      return 2^((setASize/2));
+      return 2 ^ (setASize / 2);
     }
   }
 
@@ -73,34 +73,37 @@ class StringAllPositionsAreEqual extends Factory {
   }
 
   generateOptionOne(setASize, even_or_odd) {
-    let incorrectAnswerOne = this.generateCorrectAnswer(setASize, even_or_odd) / 4;
+    let incorrectAnswerOne =
+      this.generateCorrectAnswer(setASize, even_or_odd) / 4;
     return {
-          body: String.raw`${incorrectAnswerOne}$`,
-          correct: false,
+      body: String.raw`${incorrectAnswerOne}$`,
+      correct: false,
     };
   }
 
   generateOptionTwo(setASize, even_or_odd) {
-    let incorrectAnswerTwo = this.generateCorrectAnswer(setASize, even_or_odd) / 2;
+    let incorrectAnswerTwo =
+      this.generateCorrectAnswer(setASize, even_or_odd) / 2;
     return {
-          body: String.raw`${incorrectAnswerTwo}$`,
-          correct: false,
+      body: String.raw`${incorrectAnswerTwo}$`,
+      correct: false,
     };
   }
 
   generateOptionThree(setASize, even_or_odd) {
     let correctAnswer = this.generateCorrectAnswer(setASize, even_or_odd);
     return {
-          body: String.raw`${correctAnswer}$`,
-          correct: true,
+      body: String.raw`${correctAnswer}$`,
+      correct: true,
     };
   }
 
   generateOptionFour(setASize, even_or_odd) {
-    let incorrectAnswerThree = this.generateCorrectAnswer(setASize, even_or_odd) * 2;
+    let incorrectAnswerThree =
+      this.generateCorrectAnswer(setASize, even_or_odd) * 2;
     return {
-          body: String.raw`${incorrectAnswerThree}$`,
-          correct: false,
+      body: String.raw`${incorrectAnswerThree}$`,
+      correct: false,
     };
   }
 }
---
+++ app/src/apps/Examinations/lib/questions/SelectManyQuestions.js
@@ -0,0 +1,66 @@
+import { Question } from "../Question";
+
+class SelectManyQuestions extends Question {
+  //Array of indexes that are selected
+  selectedOptionIndex = [];
+  //Array of options that are selected
+  options = [];
+  //Int representing the number of correct answers
+  number_of_correct_answers = 0;
+
+  constructor(body, options, number_of_correct_answers) {
+    super(body);
+    this.options = this.constructOptions(options);
+    this.number_of_correct_answers = number_of_correct_answers;
+  }
+
+  constructOptions(options) {
+    return options.map(
+      (option) => new SelectManyQuestionsOption(option.body, option.correct)
+    );
+  }
+
+  /* Adds a index to the selectedOptionIndex if it's not been selected currently */
+  addSelectedOption(index) {
+    if (!this.selectedOptionIndex.includes(index)) {
+      this.selectedOptionIndex.add(index);
+    }
+  }
+
+  /* Removes an index from the selectedOptionIndex if it's inside the selectedOptionIndex array */
+  removeSelectedOption(index) {
+    if (this.selectedOptionIndex.includes(index)) {
+      this.selectedOptionIndex.remove(index);
+    }
+  }
+
+  isAnsweredCorrectly() {
+    number_of_selected_correct_answers = 0;
+
+    for (let i = 0; i < this.selectedOptionIndex.length; i++) {
+      if (!this.options[this.selectedOptionIndex[i]].correct) {
+        return false;
+      } else {
+        number_of_selected_correct_answers += 1;
+      }
+    }
+
+    if (number_of_selected_correct_answers != this.number_of_correct_answers) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+class SelectManyQuestionsOption {
+  body;
+  correct;
+
+  constructor(body, correct) {
+    this.body = body;
+    this.correct = correct;
+  }
+}
+
+export { SelectManyQuestions };
