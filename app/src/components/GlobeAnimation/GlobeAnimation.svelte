41cb3de5a1ff3a0566d63529a5ce2715ef3979d7
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-08-12 14:18:54 -0400

    Add GlobeAnimation component to the home page

---
+++ app/src/components/GlobeAnimation/GlobeAnimation.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from "svelte";
+
+  let LottiePlayer;
+
+  onMount(async () => {
+    const module = await import("@lottiefiles/svelte-lottie-player");
+    LottiePlayer = module.LottiePlayer;
+  });
+</script>
+
+{#if LottiePlayer}
+  <div class="lottie">
+    <LottiePlayer
+      src="/lottie/animation.json"
+      autoplay={true}
+      loop={true}
+      controls={false}
+      renderer="svg"
+      background="transparent"
+      height={950}
+      width={950}
+    />
+  </div>
+{/if}
