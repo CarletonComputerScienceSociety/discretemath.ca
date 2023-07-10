dd8f8791a799984d21f5c765079136947704b6ab
j-tad <jacobtadeson50@gmail.com>
2023-07-09 20:39:48 -0400

    Add content card

---
+++ app/src/components/ContentCard/ContentCard.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let title;
+  export let info;
+  export let tag;
+  import "./styles.scss";
+</script>
+
+<div class="content-card">
+  <div class="content-card-tag">
+    {tag}
+  </div>
+  <div class="content-card-title">{title}</div>
+  <!-- <div class="content-card-info">
+      <span class="content-card-info-spacing" />{info}
+    </div> -->
+</div>
--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,3 +1,4 @@
 export { Button } from "./Button";
+export { ContentCard } from "./ContentCard";
 export { CourseNavbar } from "./CourseNavbar";
 export { LoadingAnimation } from "./LoadingAnimation";
